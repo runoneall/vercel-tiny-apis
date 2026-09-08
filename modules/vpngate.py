197c6c9867b85353a27564f6679df9af7cfb58d5
runoneall <runoobsteve@gmail.com>
2026-09-08 14:03:13 +0800

    Fix VPN data filtering to skip lines starting with asterisks

--- modules/vpngate.py
+++ modules/vpngate.py
@@ -24,7 +24,7 @@ class Module:
         for raw_line in resp.iter_lines():
             if raw_line:
                 line = raw_line.decode("utf-8")
-                if not line.startswith("public-vpn-"):
+                if line.startswith("*"):
                     continue
 
                 parts = line.split(",")
