78a14d1a6b9bfa9ae30439c9c4b1d585a6b3cfda
runoneall <runoobsteve@gmail.com>
2026-09-08 13:02:51 +0800

    Refactor vpngate module: update API method and enhance type definitions for VPN items

--- modules/vpngate.py
+++ modules/vpngate.py
@@ -1,15 +1,24 @@
+from typing import TypedDict
+
 from flask import Blueprint, jsonify
 import requests
+
+
+class VPNItem(TypedDict):
+    hostname: str
+    ip: str
+    ping: int
+    speed: int
 
 
 class Module:
     scope = "vpngate"
 
     def __init__(self, router: Blueprint) -> None:
-        router.get("/")(self.vpngate)
+        router.get("/")(self.api)
 
-    def vpngate(self):
-        vpns: list[dict[str, str]] = []
+    def api(self):
+        vpns: list[VPNItem] = []
 
         resp = requests.get(url="https://www.vpngate.net/api/iphone", stream=True)
         for raw_line in resp.iter_lines():
@@ -21,9 +30,12 @@ class Module:
                 parts = line.split(",")
                 vpns.append(
                     {
-                        "name": parts[0],
-                        "addr": parts[1],
+                        "hostname": parts[0],
+                        "ip": parts[1],
+                        "ping": int(parts[3]),
+                        "speed": int(parts[4]),
                     }
                 )
 
+        vpns.sort(key=lambda x: x["speed"] / (x["ping"] + 10), reverse=True)
         return jsonify(vpns)
